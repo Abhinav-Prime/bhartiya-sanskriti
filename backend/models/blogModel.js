8a2502866c205b907c1e334ca46ee03f63a004df
Abhinav Prime <abhinavprime835@gmail.com>
2024-08-15 00:45:14 +0530

    updated blog model

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -1,22 +1,53 @@
 import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  name: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  profile: { type: String, required: true },
-  likes: [{ type: String, default: "" }],
-  likeCount: { type: Number, default: 0 },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  profile: {
+    type: String,
+    requird: true,
+  },
+  date: {
+    type: Date,
+    default: date.now,
+  },
+  likes: {
+    type: String,
+    default: "",
+  },
+  likesCount: {
+    type: String,
+    default: 0,
+  },
   comments: [
     {
-      profile: { type: String, required: true },
-      name: { type: String, required: true },
-      comment: { type: String, required: true },
-      date: { type: Date, default: Date.now },
+      profileImage: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
     },
   ],
 });
-//blog models
-
-const blogModel = mongoose.models.blog || mongoose.model("blog", blogSchema);
-export default blogModel;
+const blogModel = mongoose.model("blog", blogSchema);
+export { blogModel };
